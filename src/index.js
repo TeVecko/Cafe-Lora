cfcf10f7b8daecb17763153159b8928e0e9e66e1
Tereza Vecková <te.veckova@gmail.com>
2021-04-24 14:03:39 +0200

    Zprovoznění navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './style.css';
 
 console.log('funguju!');
+
+const menu = document.querySelector('#nav-btn');
+
+menu.addEventListener('click', (event) => {
+  const navigace = document.querySelector('nav');
+  navigace.classList.toggle('nav-closed');
+});
+
+const polozkyMenu = document.querySelectorAll('.navigation a');
+
+polozkyMenu.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    console.log('Element', element);
+    const navigace = document.querySelector('nav');
+    navigace.classList.toggle('nav-closed');
+  });
+});
+
+// const objednat = document.querySelector('.order-btn');
+// objednat.addEventListener('click', (event) => {
+//   console.log('Stisknuto');
+// })
